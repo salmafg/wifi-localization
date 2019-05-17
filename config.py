fee8b3baeab2021eb36efc9cd207f67c5f50a4e5
Salma <salmagalal94@gmail.com>
2019-05-17 14:35:17 +0200

    modified config file

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ TRILATERATION = {
     'APs': [
         {
             'id': 21,
-            'start_timestamp': '16 May 2019 15:07',
-            'end_timestamp': '16 May 2019 15:10',
-            'xyz': (0, 0, 0)
+            'xy': (0, 0, 0),
+            'start_timestamp': '16 May 2019 15:02',
+            'end_timestamp': '16 May 2019 15:05'
         },
         {
             'id': 55,
-            'start_timestamp': '16 May 2019 15:07',
-            'end_timestamp': '16 May 2019 15:10',
-            'xyz': (-0.676, 3.337, -0.313)
+            'xy': (-0.676, 3.337, 0),
+            'start_timestamp': '16 May 2019 15:02',
+            'end_timestamp': '16 May 2019 15:05'
         },
         {
             'id': 57,
-            'start_timestamp': '16 May 2019 15:07',
-            'end_timestamp': '16 May 2019 15:10',
-            'xyz': (3.077, 0.877, -0.313)
+            'xy': (3.077, 0.877, 0),
+            'start_timestamp': '16 May 2019 15:02',
+            'end_timestamp': '16 May 2019 15:05'
         }
     ]
 }
